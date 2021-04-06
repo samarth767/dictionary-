6c14f7eecdd170b5db240755b2f8162c331c7b70
samarth767 <63947003+samarth767@users.noreply.github.com>
2021-04-06 22:18:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class App extends React.Component {}
